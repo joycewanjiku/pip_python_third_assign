31e8f09015e681cc00ce503d4de1defae18926fc
student <joycemumbiiwanjiku@gmail.com>
2023-05-09 22:31:37 +0300

    br

---
+++ pip_pyhon_quiz.py
@@ -0,0 +1,55 @@
+# Write a Python program that takes a list of strings as input and outputs
+#  the number of times each string appears in the list
+
+string_list=["Apple","Mango","Banana"]
+string_count=[]
+for string in string_list:
+    if string in string_count:
+            string_count(string)
+    else:
+          string_count(string)
+          for string, count in string_count()
+          print(string_count)
+
+#  using a dictionary and conditional statements.
+# Write a Python program that takes a list of numbers as input and 
+# outputs the median of the numbers 
+num_list=[2,5,6,7,8,6]
+sorted_list=sorted[num_list]
+length=len(num-10)
+if length %==0
+median=(sorted_list[length//2-1])
+sorted_list[length//2]/2
+else:
+median=(sorted_list[length//2])
+print("The median is.Median")
+
+# Write a Python program that takes a list of numbers as input and
+#  outputs the second largest number in the list using conditional
+#  statements and a for loop.
+number_list=[45,78,90,87,23]
+largest=number_list[0]
+for num in number_list:
+    if number > largest:
+          second_largest=largest
+          largest=number
+          if number>second_largest:
+                largest:
+                second_largest
+      
+# Write a Python program that takes a year as input and determines 
+# if it is a leap year.
+   year=2023
+if year % 4==0:
+      if year % 100==0:
+            if year % 400== % 100==0:
+                  if year %400==0:
+                        print("year is leap year")
+                  else:
+                        print("year is leap year")
+                  else:
+print("year is not a leap year")
+                  
+# Write a Python program that takes a string as input and checks 
+# if it is a palindrome (reads the same forwards and backwards),
+#  ignoring spaces and punctuation.
